90af1e7c3cd824a00cd5c2f5417c6e0779f7dc57
Trugamr <trugamr@live.in>
2022-10-04 20:38:54 +0530

    feat: add menu with logout option

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,10 +1,13 @@
 import type { NavLinkProps } from '@remix-run/react'
+import { Form } from '@remix-run/react'
 import { Link, NavLink, Outlet } from '@remix-run/react'
 import classNames from 'classnames'
-import { FiGlobe, FiServer, FiSettings } from 'react-icons/fi'
+import { FiGlobe, FiServer, FiSettings, FiUser } from 'react-icons/fi'
 import type { IconType } from 'react-icons'
 import { requireUserId } from '~/utils/session.server'
 import type { LoaderArgs } from '@remix-run/node'
+import { Menu } from '@headlessui/react'
+import { Float } from '@headlessui-float/react'
 
 export const loader = ({ request }: LoaderArgs) => {
   return requireUserId(request)
@@ -16,10 +19,40 @@ export default function AdminRoute() {
       <div className="mb-6 bg-gray-100 pt-4">
         <div className="container mx-auto">
           <div>
-            <header className="mb-4 flex md:mb-6">
+            <header className="mb-4 flex items-center justify-between md:mb-6">
               <Link to="/admin">
                 <h1 className="text-lg font-semibold">headscale</h1>
               </Link>
+
+              <Menu>
+                <Float
+                  placement="bottom-end"
+                  offset={8}
+                  enter="transition-opacity duration-75"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                >
+                  <Menu.Button
+                    as="button"
+                    className="rounded-full border border-gray-200 bg-white p-2 hover:border-gray-300 focus:outline-none focus:ring focus:ring-primary-300"
+                  >
+                    <FiUser className="text-lg" />
+                  </Menu.Button>
+                  <Menu.Items
+                    as={Form}
+                    method="post"
+                    action="/logout"
+                    className="w-40 rounded-md border border-gray-200 bg-white py-1 text-base shadow-lg focus:outline-none"
+                  >
+                    <Menu.Item
+                      as="button"
+                      className="w-full truncate text-ellipsis px-3 py-1.5 text-left ui-active:bg-gray-100 ui-disabled:text-gray-300"
+                    >
+                      Log out
+                    </Menu.Item>
+                  </Menu.Items>
+                </Float>
+              </Menu>
             </header>
           </div>
           <nav className="flex gap-x-2.5 md:gap-x-4">
